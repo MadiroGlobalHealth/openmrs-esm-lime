b5280f36677527f6b8dce1210ddca5908ac80769
Pius Rubangakene <piruville@gmail.com>
2024-09-23 21:09:35 +0300

    (fix) LIME2-380 Enable overflow scroll in nutrition summary

--- packages/esm-nutrition-app/src/nutrition/nutrition-summary.component.tsx
+++ packages/esm-nutrition-app/src/nutrition/nutrition-summary.component.tsx
@@ -19,12 +19,12 @@ import { Add } from '@carbon/react/icons';
 import { CardHeader, ErrorState } from '@openmrs/esm-patient-common-lib';
 import { formatDate, useLayoutType, isDesktop as desktopLayout } from '@openmrs/esm-framework';
 import { EmptyState } from '../empty-state/empty-state.component';
-import styles from './nutrition-summary.scss';
 import { usePatientNutrition } from '../hooks/nutrition.resource';
 import { launchClinicalViewForm, mealSymbol } from '../helpers/helpers';
 import { type Encounter } from '../types';
 import { useForm } from '../hooks/form.resource';
 import { mealAmountConcepts, mealRemarkConcepts, nutritionFormUuid } from '../constants';
+import styles from './nutrition-summary.scss';
 
 interface NutritionSummaryProps {
   patientUuid: string;
@@ -67,18 +67,21 @@ const NutritionSummary: React.FC<NutritionSummaryProps> = ({ patientUuid }) => {
     ];
   }, [nutritionData, t]);
 
+  const getRowData = (encounter, mealAmountConcept, index) => {
+    let obs = encounter.obs.find((obs) => obs.concept.uuid === mealAmountConcept);
+    return {
+      mealTakenSymbol: mealSymbol(obs?.value?.name?.name ?? ''),
+      mealRemark:
+        encounter.obs.find((obs) => obs.concept.uuid === mealRemarkConcepts[index])?.value?.name?.name.charAt(0) ?? '',
+    };
+  };
+
   const tableRows = useMemo(() => {
     return mealAmountConcepts.map((mealAmountConcept, index) => {
       let mealNumber = (index % 10) + 1;
       const row = { id: mealAmountConcept, encounterDate: `Meal ${mealNumber}` };
-      nutritionData?.map((encounter) => {
-        let obs = encounter.obs.find((obs) => obs.concept.uuid === mealAmountConcept);
-        row[encounter.uuid] = {
-          mealTakenSymbol: mealSymbol(obs?.value?.name?.name ?? ''),
-          mealRemark:
-            encounter.obs.find((obs) => obs.concept.uuid === mealRemarkConcepts[index])?.value?.name?.name.charAt(0) ??
-            '',
-        };
+      nutritionData?.forEach((encounter) => {
+        row[encounter.uuid] = getRowData(encounter, mealAmountConcept, index);
       });
       return row;
     });
@@ -90,7 +93,7 @@ const NutritionSummary: React.FC<NutritionSummaryProps> = ({ patientUuid }) => {
     return <EmptyState displayText={nutritionSummaryText} launchForm={launchNutritionForm} />;
   if (nutritionData?.length > 0) {
     return (
-      <div className={styles.widgetCard}>
+      <div className={styles.nutritionWidgetCard}>
         <CardHeader title={nutritionSummaryText}>
           {formIsLoading && <SkeletonText />}
           {form && (
@@ -105,87 +108,81 @@ const NutritionSummary: React.FC<NutritionSummaryProps> = ({ patientUuid }) => {
           )}
         </CardHeader>
 
-        <DataTable rows={tableRows} headers={tableHeaders} isSortable size={isTablet ? 'lg' : 'sm'} useZebraStyles>
-          {({ rows, headers, getHeaderProps, getTableProps }) => (
-            <TableContainer>
-              <Table
-                aria-label="nutrition summary"
-                {...getTableProps()}
-                className={classNames(styles.nutritionTableCell)}
-              >
-                <TableHead>
-                  <TableRow>
-                    {headers.map((header, index) => (
-                      <TableHeader
-                        colSpan={index === 0 ? 1 : 2}
-                        className={classNames(styles.productiveHeading01, styles.text02, styles.widgetTableHeader)}
-                        {...getHeaderProps({
-                          header,
-                          isSortable: false,
-                        })}
-                      >
-                        {index === 0 ? (
-                          header.header?.content ?? header.header
-                        ) : (
-                          <Link
-                            style={{ cursor: 'pointer' }}
-                            onClick={(e) => {
-                              e.preventDefault();
-                              editNutritionEncounterForm(header.key);
-                            }}
-                          >
-                            {header.header?.content ?? header.header}
-                          </Link>
-                        )}
-                      </TableHeader>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow key={row.id}>
-                      {row.cells.map((cell, index) =>
-                        index === 0 ? (
-                          <TableCell key={cell.id}>{cell.value?.content ?? cell.value}</TableCell>
-                        ) : (
-                          <>
-                            <TableCell
-                              className={styles.mealTaken}
-                              key={cell.id}
-                              data-status={cell.value?.content?.mealTakenSymbol ?? cell.value?.mealTakenSymbol}
+        <div className={styles.tableContainer}>
+          <DataTable rows={tableRows} headers={tableHeaders} isSortable size={isTablet ? 'lg' : 'sm'} useZebraStyles>
+            {({ rows, headers, getHeaderProps, getTableProps }) => (
+              <TableContainer>
+                <Table
+                  aria-label="nutrition summary"
+                  {...getTableProps()}
+                  className={classNames(styles.nutritionTableCell)}
+                >
+                  <TableHead>
+                    <TableRow>
+                      {headers.map((header, index) => (
+                        <TableHeader
+                          colSpan={index === 0 ? 1 : 2}
+                          className={classNames(
+                            styles.productiveHeading01,
+                            styles.text02,
+                            styles.widgetTableHeader,
+                            index === 0 ? styles.stickyTableColumn : '',
+                          )}
+                          {...getHeaderProps({
+                            header,
+                            isSortable: false,
+                          })}
+                        >
+                          {index === 0 ? (
+                            header.header?.content ?? header.header
+                          ) : (
+                            <Link
+                              style={{ cursor: 'pointer' }}
+                              onClick={(e) => {
+                                e.preventDefault();
+                                editNutritionEncounterForm(header.key);
+                              }}
                             >
-                              {cell.value?.content?.mealTakenSymbol ?? cell.value?.mealTakenSymbol}
-                            </TableCell>
-                            <TableCell key={cell.id}>
-                              {cell.value?.content?.mealRemark ?? cell.value?.mealRemark}
-                            </TableCell>
-                          </>
-                        ),
-                      )}
+                              {header.header?.content ?? header.header}
+                            </Link>
+                          )}
+                        </TableHeader>
+                      ))}
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )}
-        </DataTable>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow key={row.id}>
+                        {row.cells.map((cell, index) =>
+                          index === 0 ? (
+                            <TableCell key={cell.id} className={styles.stickyTableColumn}>
+                              {cell.value?.content ?? cell.value}
+                            </TableCell>
+                          ) : (
+                            <>
+                              <TableCell
+                                className={styles.mealTaken}
+                                key={cell.id}
+                                data-status={cell.value?.content?.mealTakenSymbol ?? cell.value?.mealTakenSymbol}
+                              >
+                                {cell.value?.content?.mealTakenSymbol ?? cell.value?.mealTakenSymbol}
+                              </TableCell>
+                              <TableCell key={cell.id}>
+                                {cell.value?.content?.mealRemark ?? cell.value?.mealRemark}
+                              </TableCell>
+                            </>
+                          ),
+                        )}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            )}
+          </DataTable>
+        </div>
       </div>
     );
-  }
-};
-
-const FeedTableCell = ({ interpretation, children }: { interpretation: string; children: React.ReactNode }) => {
-  switch (interpretation) {
-    case 'critically_high':
-      return <TableCell className={styles.criticallyHigh}>{children}</TableCell>;
-    case 'critically_low':
-      return <TableCell className={styles.criticallyLow}>{children}</TableCell>;
-    case 'high':
-      return <TableCell className={styles.high}>{children}</TableCell>;
-    case 'low':
-      return <TableCell className={styles.low}>{children}</TableCell>;
-    default:
-      return <TableCell>{children}</TableCell>;
   }
 };
 
