f35472859ab95dfcc86da557c05df696ad553c34
Pius Rubangakene <piruville@gmail.com>
2025-06-18 01:43:15 -0700

    (fix) LIME2-623 Update required privilege to utilize useHasAccess hook (#35)
    * (fix) LIME2-623 Update required privilege endpoint
    
    * refactors to customHasRequiredPrivilege to utilize useHasAccess hook

--- packages/esm-mental-health-app/src/utils/helpers.ts
+++ packages/esm-mental-health-app/src/utils/helpers.ts
@@ -1,16 +1,9 @@
-import { fhirBaseUrl, openmrsFetch, restBaseUrl } from '@openmrs/esm-framework';
+import { fhirBaseUrl, openmrsFetch, restBaseUrl, userHasAccess, useSession } from '@openmrs/esm-framework';
 
-export const hasRequiredPrivilege = async (privilege: string): Promise<boolean> => {
-  const privileges = await fetchPrivileges();
-  return privileges.includes(privilege);
-};
-
-async function fetchPrivileges(): Promise<Array<string>> {
-  const response = await openmrsFetch(`${restBaseUrl}/session`, {
-    method: 'GET',
-  });
-  const data = await response.json();
-  return data?.user?.privileges?.map((p) => p.display) || [];
+export function useHasRequiredPrivilege(privilege: string): boolean {
+  const session = useSession();
+  const hasPrivilege = userHasAccess(privilege, session?.user);
+  return !!hasPrivilege;
 }
 
 export function getTotalPatientEncounters(patientUuid: string, encounterTypeUuid: string, formName?: string) {
