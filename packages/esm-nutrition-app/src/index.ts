b7f55e0e8373ea0ad64c349169be00fb0edefc9c
Pius Rubangakene <piruville@gmail.com>
2025-07-05 22:43:23 +0300

    (fix) removes deprecated registerNavGroup

--- packages/esm-nutrition-app/src/index.ts
+++ packages/esm-nutrition-app/src/index.ts
@@ -3,7 +3,6 @@ import { configSchema } from './config-schema';
 import { createDashboardLink as createPatientChartDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { patientChartDashboardMeta } from './dashboard.meta';
 import NutritionSummary from './nutrition/nutrition-summary.component';
-import ClinicalViewDivider from './clinical-views/clinical-view-divider.component';
 import { registerExpressionHelper } from '@openmrs/esm-form-engine-lib';
 import { getPatientEncounterDates } from './utils/helpers';
 
@@ -21,8 +20,6 @@ export function startupApp() {
 
   registerExpressionHelper('getPreviousEncounterDates', getPatientEncounterDates);
 }
-
-export const clinicalViewPatientDivider = getSyncLifecycle(ClinicalViewDivider, options);
 
 // t('Nutrition', 'Nutrition')
 export const patientNutritionSummaryDashboardLink = getSyncLifecycle(
