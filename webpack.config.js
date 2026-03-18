cd0d547744622b82e82b9b3a417fd0f43a6dcffe
Pius Rubangakene <piruville@gmail.com>
2026-03-18 16:00:44 +0300

    (chore) Update dependencies and remove unused (#41)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,1 @@
-const config = (module.exports = require('openmrs/default-webpack-config'));
-config.scriptRuleConfig.exclude = /(node_modules(?![\/\\]@(?:openmrs)))/;
-
-module.exports = config;
+module.exports = require('openmrs/default-webpack-config');
